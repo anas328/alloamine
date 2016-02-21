5d08ba40a48fd6a99c95ceacf6f06a70aea5e562
Lithium2 <philippe.baala@gmail.com>
2016-02-21 18:45:49 +0100

    mongodb database access

--- listener.js
+++ listener.js
@@ -1,34 +1,28 @@
 // API IMPORTS //
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongojs = require("mongojs");
 
 // API INSTANTIATIONS //
 var express_app		= express();
+var db = mongojs('ALLO_AMINE', ['district','category','product']);
 
 // PATH PROVIDES SERVER //
 express_app.use('/public', express.static(__dirname + "/public"));
 express_app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 express_app.use(bodyParser.json());
 
+// var categories = [
+// 	{ _id:"1", name:"PIZZAS", image : "photo.jpg" },
+// 	{ _id:"2", name:"PATES", image : "photo.jpg"  },
+// 	{ _id:"3", name:"DESSERTS", image : "photo.jpg"  }
+// ];
 
-// VARIABLES - TMP //
-var districts = [
-	{ _id:"1", name:"Creteil-Université" },
-	{ _id:"2", name:"Boulogne" },
-	{ _id:"3", name:"République" }
-];
-
-var categories = [
-	{ _id:"1", name:"PIZZAS", image : "photo.jpg" },
-	{ _id:"2", name:"PATES", image : "photo.jpg"  },
-	{ _id:"3", name:"DESSERTS", image : "photo.jpg"  }
-];
-
-var products = [
-	{ _id:"10", name:"pizza_marguarita", id_cat:"1", image:"photo.jpg",prix:"100"},
-	{ _id:"11", name:"spaghetti_bolonaise",id_cat:"2", image:"photo.jpg",prix:"100"},
-	{ _id:"12", name:"glace",id_cat:"3", image:"photo.jpg",prix:"100"}
-];
+// var products = [
+// 	{ _id:"10", name:"pizza_marguarita", id_cat:"1", image:"photo.jpg",prix:"100"},
+// 	{ _id:"11", name:"spaghetti_bolonaise",id_cat:"2", image:"photo.jpg",prix:"100"},
+// 	{ _id:"12", name:"glace",id_cat:"3", image:"photo.jpg",prix:"100"}
+// ];
 
 // ROOT DEFINITIONS //
 
@@ -40,26 +34,34 @@ express_app.get('/', function(req,res){
 
 //get quartier function
 express_app.get('/districts', function(req,res){
-	console.log("Recuperation des quartiers");
-	console.log(districts);
-	res.json(districts);
+	db.district.find(function(err, docs) {
+		console.log("Recuperation des quartiers");
+		console.log(docs);
+		res.json(docs);
+	});
 });
 
 //get produits function
 express_app.get('/categories', function(req,res){
-	console.log("Recuperation des categories");
-	console.log(categories);
-	res.json(categories);
+	db.category.find(function(err, docs) {
+		console.log("Recuperation des categories");
+		console.log(docs);
+		res.json(docs);
+	});
 });
 
 
 //get produits function
-express_app.get('/products', function(req,res){
-	console.log("Recuperation des produits");
-	console.log(products);
-	res.json(products);
+express_app.post('/products', function(req,res){
+	// category = req.body.category;
+	
+	category = { _id:mongojs.ObjectId("56c9ec79d8dc9751d945611b"), name:"PIZZAS", image : "photo.jpg" };
+	db.product.find({"id_cat":category._id}, function(err, docs) {
+		// console.log("Recuperation des produits de la categorie %s", category._id);
+		console.log(docs);
+		res.json(docs);
+	});
 });
-
 
 // SERVER RUNNING //
 var server = express_app.listen(7000, function() {
